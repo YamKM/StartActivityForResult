aa3f7c325a3f1856252d8c62093aebf6525ea3af
YamKM <yam.m.kolel@gmail.com>
2025-12-02 17:11:27 +0200

    Add SettingActivity

--- app/src/main/java/com/example/startactivityforresultproy/SettingActivity.java
+++ app/src/main/java/com/example/startactivityforresultproy/SettingActivity.java
@@ -1,31 +1,24 @@
 package com.example.startactivityforresultproy;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
-import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity {
 
-    Button btnSetting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_setting);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        btnSetting = findViewById(R.id.btnSetting);
-        ActivityResultLauncher<Intent> resultLauncher;
-
     }
 }
