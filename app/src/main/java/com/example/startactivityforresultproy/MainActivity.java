79a09d31e844db4d56b68aacb2d61a6428d1a07c
YamKM <yam.m.kolel@gmail.com>
2026-01-11 17:09:27 +0200

    Launch

--- app/src/main/java/com/example/startactivityforresultproy/MainActivity.java
+++ app/src/main/java/com/example/startactivityforresultproy/MainActivity.java
@@ -2,18 +2,23 @@ package com.example.startactivityforresultproy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnSetting;
+    ActivityResultLauncher<Intent> resultLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +30,21 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         btnSetting = findViewById(R.id.btnSetting);
-        ActivityResultLauncher<Intent> resultLauncher;
+        btnSetting.setOnClickListener(this);
+        resultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
 
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent =new Intent(this, SettingActivity.class);
+        resultLauncher.launch(intent);
     }
 }
